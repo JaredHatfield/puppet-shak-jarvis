9cd094bc244f292f67ecd6723c2d85799e994785
JaredHatfield <jjhatf02@gmail.com>
2014-08-13 21:00:36 -0400

    Transcribed shak and jarvis

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,26 +10,26 @@ class shak-jarvis(
   $dbpassword) {
 
   class { 'jsvc::deamon':
-    jsvc_name       => 'jarvis-shak',
-    jsvc_class_path => "/etc/jarvis-shak/shak-jarvis.jar",
+    jsvc_name       => 'shak-jarvis',
+    jsvc_class_path => "/etc/shak-jarvis/shak-jarvis.jar",
     jsvc_class      => "com.unitvectory.shak.jarvis.App",
     jsvc_java_home  => $java_path,
   }
 
-  file { "/etc/jarvis-shak/shak-jarvis.jar":
+  file { "/etc/shak-jarvis/shak-jarvis.jar":
     ensure => file,
     owner  => root,
     group  => root,
     mode   => 755,
     source => $app_file,
-    notify => Service['jarvis-shak'],
+    notify => Service['shak-jarvis'],
   }
 
-  file { "/etc/jarvis-shak/config.xml":
+  file { "/etc/shak-jarvis/config.xml":
     owner   => root,
     group   => root,
     mode    => 644,
     content => template("shak-jarvis/config.xml.erb"),
-    notify  => Service['jarvis-shak'],
+    notify  => Service['shak-jarvis'],
   }
 }
